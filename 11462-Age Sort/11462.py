71fffa6adbd02bd4804bb6118f5f9b386df5064d
EddieChuang <az564123@gmail.com>
2017-07-08 12:29:38 +0800

    11462-Age Sort.py
    Verdict: Runtime Error

---
+++ 11462-Age Sort/11462.py
@@ -0,0 +1,31 @@
+def minHeapify(ages, root, n):
+    left = 2 * root + 1
+    right = 2 * (root + 1)
+    if left < n:
+        minHeapify(ages, left, n)
+        if ages[root] > ages[left]:
+            swap(ages, root, left)
+    if right < n:
+        minHeapify(ages, right, n)
+        if ages[root] > ages[right]:
+            swap(ages, root, right)
+    
+def heapSort(ages, n):
+    for i in range(0, n):
+        minHeapify(ages, 0, n - i)
+        print(ages[0], end=' ')
+        swap(ages, 0, n - i - 1)
+
+def swap(ages, x, y):
+    ages[x], ages[y] = ages[y], ages[x]
+
+n = int(input())  
+while n != 0:
+
+    ages = list()
+    for i in range(0, n):
+        ages.append([age for age in input()])
+
+    heapSort(ages, len(ages))   
+    print ()
+    n = int(input())
